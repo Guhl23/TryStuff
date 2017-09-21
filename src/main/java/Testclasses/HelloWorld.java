e7ce58de10d032b82092ea12c732c428a63e44b1
Guhl23 <flavianburkhard@gmail.com>
2017-09-21 16:27:44 +0200

    Testclass

---
+++ src/main/java/Testclasses/HelloWorld.java
@@ -0,0 +1,10 @@
+package Testclasses;
+
+public class HelloWorld {
+
+    static String test = "Hello World";
+
+    public static void main(String[] args) {
+        System.out.println(test);
+    }
+}
